9f723ed09367b2dcd3e91992c78730e859c50f28
Edward Ng <edwardlng@outlook.com>
2023-07-09 18:00:55 -0400

    Created Face Model

---
+++ src/model/Face.js
@@ -0,0 +1,52 @@
+import Circle from './geometry/Circle';
+import SemiCircle from './geometry/SemiCircle';
+
+class Face {
+  constructor(centerX, centerY, radius, velocityX, velocityY, color, eyeMouthColor) {
+    this.head = new Circle(centerX, centerY, radius, color);
+
+    this.velocity = {
+      x: velocityX,
+      y: velocityY,
+    };
+
+    this.eyes = {
+      left: new Circle(
+        this.head.center.x - (3 / 8) * this.radius,
+        this.head.center.y,
+        this.head.radius / 8,
+        eyeMouthColor,
+      ),
+      right: new Circle(
+        this.head.center.x + (3 / 8) * this.radius,
+        this.head.center.y,
+        this.head.radius / 8,
+        eyeMouthColor,
+      ),
+    };
+
+    this.mouth = new SemiCircle(
+      this.head.center.x,
+      this.head.center.y,
+      this.head.radius / 4,
+      eyeMouthColor,
+      180,
+    );
+  }
+
+  /**
+   * Generates the circles and semicircles composing the face.
+   *
+   * @generator
+   * @function getCircles
+   * @yields {Circle} - The next circle or semicircle composing the face.
+   */
+  * getCircles() {
+    yield this.head;
+    yield this.eyes.left;
+    yield this.eyes.right;
+    yield this.mouth;
+  }
+}
+
+export default Face;
